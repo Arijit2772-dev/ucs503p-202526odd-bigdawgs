35ff4b31d2304b48e819a2c930cd1373fc3cc1eb
biggy27 <workspace2772@gmail.com>
2025-10-29 18:07:15 +0530

    api

--- app.py
+++ app.py
@@ -1,33 +1,80 @@
+import streamlit as st
+import pickle
 import numpy as np
 import pandas as pd
-from flask import Flask, request, jsonify
-import pickle
 
-# Initialize the flask app
-app = Flask(__name__)
+# import the model
+pipe = pickle.load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
 
-# Load the trained pipeline
-pipe = pickle.load(open('pipe.pkl', 'rb'))
+st.title("Laptop Predictor")
 
-# Define a route for the prediction
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get the data from the POST request
-    data = request.get_json(force=True)
+# brand
+company = st.selectbox('Brand',df['Company'].unique())
 
-    # Convert the incoming JSON data to a pandas DataFrame
-    # The keys in your JSON ('Company', 'TypeName', etc.) must match these columns
-    input_data = pd.DataFrame([data])
+# type of laptop
+type = st.selectbox('Type',df['TypeName'].unique())
+
+# Ram
+ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+
+# weight
+weight = st.number_input('Weight of the Laptop')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+
+# IPS
+ips = st.selectbox('IPS',['No','Yes'])
+
+# screen size
+screen_size = st.slider('Screensize in inches', 10.0, 18.0, 13.0)
+
+# resolution
+resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+
+#cpu
+cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+
+ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+
+gpu = st.selectbox('GPU',df['Gpu_Brand'].unique())
+
+os = st.selectbox('OS',df['os'].unique())
+
+if st.button('Predict Price'):
+    # query
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
     
-    # Make a prediction
-    prediction_log = pipe.predict(input_data)[0]
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
     
-    # Since you trained on the log of the price, convert it back
-    prediction = np.exp(prediction_log)
-
-    # Return the result as JSON
-    return jsonify({'predicted_price': prediction})
-
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
+    
+    # Creating a DataFrame instead of numpy array
+    query = pd.DataFrame({
+        'Company': [company],
+        'TypeName': [type],
+        'Ram': [ram],
+        'Weight': [weight],
+        'Touchscreen': [touchscreen],
+        'Ips': [ips],
+        'ppi': [ppi],
+        'Cpu brand': [cpu],
+        'HDD': [hdd],
+        'SSD': [ssd],
+        'Gpu_Brand': [gpu],
+        'os': [os]
+    })
+    
+    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
